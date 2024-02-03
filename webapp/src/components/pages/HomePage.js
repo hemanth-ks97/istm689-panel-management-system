a12175efb1ea59992e0b9f57fc020d3fc6910a00
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-03 13:30:51 -0600

    auto deploy

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -6,7 +6,7 @@ import Box from "@mui/material/Box";
 const HomePage = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      This is the HomePage component. Test autodeploy
+      This is the HomePage component. Test autodeploy 1
     </Box>
   );
 };
