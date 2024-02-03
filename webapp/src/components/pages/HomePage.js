b1795d3d83f4041eb89105814f0ee9e276551767
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-03 11:56:03 -0600

    test autodeploy

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -4,7 +4,11 @@ import React from "react";
 import Box from "@mui/material/Box";
 
 const HomePage = () => {
-  return <Box sx={{ flexGrow: 1 }}>This is the HomePage component</Box>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      This is the HomePage component. Test Amplify auto-deploy
+    </Box>
+  );
 };
 
 export default HomePage;
