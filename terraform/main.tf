dea03e9c674948033188bdb1b2605edb36688a0c
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-03 11:34:49 -0600

    test amplify app

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,21 @@ locals {
     dev = 1
     prod = 2
   }
+
+  amplify_app_repository = "https://github.com/JoaquinGimenez1/istm689-panel-management-system"
+  # needs to be a secret
+  amplify_app_oauth_token = "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"
+
+  amplify_branch_branch_name = {
+    dev = "dev"
+    prod = "main"
+  }
+
+  amplify_domain_association_domain_name = {
+    dev = "awsamplifyapp.com"
+    prod = "awsamplifyapp.com"
+  }
+
 }
 
 
@@ -75,6 +90,28 @@ resource "aws_budgets_budget" "general-budget" {
   }
 }
 
+resource "aws_amplify_app" "frontend-app" {
+  name       = "${terraform.workspace}-web-app"
+  repository = local.amplify_app_repository
+  oauth_token = local.amplify_app_oauth_token
+}
+resource "aws_amplify_branch" "frontend-branch" {
+  app_id      = aws_amplify_app.frontend-app.id
+  branch_name = local.amplify_branch_branch_name[terraform.workspace]
+}
+resource "aws_amplify_domain_association" "domain_association" {
+  app_id      = aws_amplify_app.frontend-app.id
+  domain_name = local.amplify_domain_association_domain_name[terraform.workspace]
+  wait_for_verification = false
+
+  sub_domain {
+    branch_name = local.amplify_branch_branch_name[terraform.workspace]
+    prefix      = terraform.workspace
+  }
+
+}
+
+
 # # Great test table. Deleting now
 # resource "aws_dynamodb_table" "gamesscores-test-dynamodb-table" {
 #   name           = "${terraform.workspace}-GameScores"
@@ -86,4 +123,14 @@ resource "aws_budgets_budget" "general-budget" {
 #     name = "UserId"
 #     type = "S"
 #   }
+# }
+
+
+# # OUTPUT #
+# output "amplify_app_id" {
+#   value = aws_amplify_app.amplify_app.id
+# }
+
+# output "amplify_app_url" {
+#   value = aws_amplify_domain_association.domain_association.domain_name
 # }
