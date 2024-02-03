1a9a12f65e4b3fd1b1895a76c4aa792715dca359
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-03 15:59:01 -0600

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -220,7 +220,7 @@ output "amplify_app_url" {
 }
 
 output "amplify_app_dns_record" {
-  value = aws_amplify_domain_association.frontend-domain-association.sub_domain[dns_record]
+  value = aws_amplify_domain_association.frontend-domain-association.sub_domain[0].dns_record
 }
 
 # output "amplify_app_dns_record_list" {
