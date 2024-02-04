d49f119b98f3fbc62ed4b167435ef0d06923af17
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-04 09:14:40 -0600

    quick test

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -1,15 +1,15 @@
 import React from "react";
 
 // MUI
-// import Box from "@mui/material/Box";
-import { Typography, Box } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const HomePage = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Typography variant="h4">This is the HomePage component.</Typography>
       <Typography variant="body2">
-        Enviroment variable: {process.env.REACT_APP_ENV || "not defined"}
+        Enviroment: {process.env.REACT_APP_ENV || "not defined"}
       </Typography>
 
       <Typography variant="body2">
