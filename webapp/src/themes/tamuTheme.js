5b73b45ced3297d6a1d114fa577f5d4001801491
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-04 15:29:15 -0600

    Add prod and dev env code

--- webapp/src/themes/tamuTheme.js
+++ webapp/src/themes/tamuTheme.js
@@ -3,6 +3,6 @@ import { createTheme } from "@mui/material/styles";
 export const tamuTheme = createTheme({
   palette: {
     primary: { main: "#500000" },
-    secondary: { main: "#3C0000" },
+    secondary: { main: "#003C71" },
   },
 });
