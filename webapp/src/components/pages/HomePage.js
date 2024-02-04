ed171ecc06ceeba5f13d51dd8a4b3cc60d026588
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-03 20:09:27 -0600

    Add some yptohgraphy

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -1,14 +1,20 @@
 import React from "react";
 
 // MUI
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
+import { Typography, Box } from "@mui/material";
 
 const HomePage = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <p>This is the HomePage component. Autodeploy with custom domain</p>
-      Enviroment variable: {process.env.REACT_APP_ENV || "not defined"}
-      Git commit: {process.env.REACT_APP_GIT_COMMIT_SHA || "not available"}
+      <Typography variant="h4">This is the HomePage component.</Typography>
+      <Typography variant="body2">
+        Enviroment variable: {process.env.REACT_APP_ENV || "not defined"}
+      </Typography>
+
+      <Typography variant="body2">
+        Git commit: {process.env.REACT_APP_GIT_COMMIT_SHA || "not available"}
+      </Typography>
     </Box>
   );
 };
