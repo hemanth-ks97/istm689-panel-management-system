84d285f7d3857b5b99e2f01d409f652997b0f7ba
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-04 20:06:59 -0600

    asdf

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -7,7 +7,7 @@ import Typography from "@mui/material/Typography";
 const HomePage = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Typography variant="h4">This is the HomePage component.</Typography>
+      <Typography variant="h4">This is the HomePage component...</Typography>
       <Typography variant="body2">
         Enviroment: {process.env.REACT_APP_ENV || "not defined"}
       </Typography>
