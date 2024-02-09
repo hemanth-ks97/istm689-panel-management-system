1613baeec90c88077d135d8d5ddc51cd662fded5
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-09 11:02:11 -0600

    Delete rest endpoint to test autodeploy on deb

--- backend/pms-core/app.py
+++ backend/pms-core/app.py
@@ -18,12 +18,12 @@ def hello_name(name):
    # '/hello/james' -> {"hello": "james"}
    return {'hello': name}
 
-@app.route('/users', methods=['POST'])
-def create_user():
-    # This is the JSON body the user sent in their POST request.
-    user_as_json = app.current_request.json_body
-    # We'll echo the json body back to the user in a 'user' key.
-    return {'user': user_as_json}
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
 
 # See the README documentation for more examples.
 #
