1077914fa1983cef8e5539c563e0304f4a2107a7
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-09 10:35:26 -0600

    Testing Github action for chalice

--- backend/pms-core/app.py
+++ backend/pms-core/app.py
@@ -13,17 +13,17 @@ def index():
 #
 # Here are a few more examples:
 #
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
+@app.route('/hello/{name}')
+def hello_name(name):
+   # '/hello/james' -> {"hello": "james"}
+   return {'hello': name}
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    # This is the JSON body the user sent in their POST request.
+    user_as_json = app.current_request.json_body
+    # We'll echo the json body back to the user in a 'user' key.
+    return {'user': user_as_json}
 
 # See the README documentation for more examples.
 #
