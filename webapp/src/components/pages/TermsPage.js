56b2f5b316a8b111e7384459ba839648d454d638
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-14 10:11:29 -0600

    Add Privacy and Terms page

---
+++ webapp/src/components/pages/TermsPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// MUI
+import { Box, Typography } from "@mui/material";
+
+const TermsPage = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Typography variant="h4">This is the TermsPage component.</Typography>
+      <Typography>
+        Application terms of service link Provide users a link to your public
+        terms of service
+      </Typography>
+    </Box>
+  );
+};
+
+export default TermsPage;
