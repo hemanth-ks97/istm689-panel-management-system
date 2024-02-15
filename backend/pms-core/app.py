156453672fd78ccc09f4bd6bcf7ba80e76a52939
hks <hemanth.ks97@gmail.com>
2024-02-15 13:22:57 -0600

    Added the google jwt token auth function to app.py and listed it's dependencies in the requirements.txt file

--- backend/pms-core/app.py
+++ backend/pms-core/app.py
@@ -1,7 +1,34 @@
-from chalice import Chalice
+from chalice import Chalice, AuthResponse
+import google.auth.transport.requests
+import google.oauth2.id_token
+from google.auth import exceptions
+import os
 
 app = Chalice(app_name='pms-core')
 
+@app.authorizer()
+def google_oauth2_authorizer(auth_request):
+    # Expects token in the "Authorization" header of incoming request ---> Format: "{"Authorization": "<token>"}" 
+    token = (auth_request.token)
+    try:
+        # Validate the JWT token using Google's OAuth2 v2 API
+        request = google.auth.transport.requests.Request()
+        id_info = google.oauth2.id_token.verify_oauth2_token(token, request, os.environ.get("GOOGLE_AUTH_CLIENT_ID"))
+        
+        # Token is valid, so return an AuthResponse with routes accessible
+        return AuthResponse(routes=['*'], principal_id=id_info['sub'])
+    except exceptions.GoogleAuthError as e:
+        # Token is invalid
+        app.log.error(f"Google Auth Error: {str(e)}")
+    except Exception as e:
+        #General catch statement for unexpected errors
+        app.log.error(f"Unexpected Error: {str(e)}")
+    return AuthResponse(routes=[], principal_id='user')
+
+# Example protected route ---> REMOVE LATER
+@app.route('/protected', authorizer=google_oauth2_authorizer)
+def protectedHello():
+    return{'hello': 'from protected world'}
 
 @app.route('/')
 def index():
