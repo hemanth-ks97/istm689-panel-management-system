793ed0e33c1447bfbb370d5c7330a274f0ac4301
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-28 12:26:17 -0600

    cleanup files

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 // MUI
 import {
   Box,
-  Button,
   Typography,
-  Snackbar,
   Paper,
   Table,
   TableBody,
@@ -14,22 +12,8 @@ import {
   TableHead,
   styled,
 } from "@mui/material";
-// Redux
-import { useSelector } from "react-redux";
-// HTTP Client
-//import { httpClient } from "../../client";
-// React Router
-//import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  //const navigate = useNavigate();
-  //const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
-  // const [apiResponse, setApiResponse] = useState("");
-  //const [isApiWaiting, setIsApiWaiting] = useState(false);
-  //const [selectedHowdyFile, SetSelectedHowdyFile] = useState("");
-
-  // const { user } = useSelector((state) => state.user);
-
   const DemoPaper = styled(Paper)(({ theme }) => ({
     width: 800,
     // height: 500,
@@ -38,43 +22,9 @@ const HomePage = () => {
     textAlign: "center",
   }));
 
-  /*
-  const handleOnClick = () => {
-    setIsApiWaiting(true);
-    httpClient
-      .get("/")
-      .then((response) =>
-        setApiResponse(JSON.stringify(response?.data, null, 2))
-      )
-      .catch((err) => setApiResponse(JSON.stringify(err.message, null, 2)))
-      .finally(() => {
-        setIsSnackbarOpen(true);
-        setIsApiWaiting(false);
-      });
-  };
-  const handleOnClickUser = () => {
-    setIsApiWaiting(true);
-    httpClient
-      .get("/user", {
-        headers: {
-          Authorization: `Bearer ${user?.token}`,
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) =>
-        setApiResponse(JSON.stringify(response?.data, null, 2))
-      )
-      .catch((err) => setApiResponse(JSON.stringify(err.message, null, 2)))
-      .finally(() => {
-        setIsApiWaiting(false);
-        setIsSnackbarOpen(true);
-      });
-  };
-*/
-
-  function createData(Assignment, Deadline) {
+  const createData = (Assignment, Deadline) => {
     return { Assignment, Deadline };
-  }
+  };
 
   const ThisWeekRows = [
     createData("Module 1 Video", "-"),
@@ -163,25 +113,6 @@ const HomePage = () => {
           </TableContainer>
         </DemoPaper>
       </Box>
-      {/*
-      <Typography>Test API Call to backend!</Typography>
-      <Button
-        variant="contained"
-        onClick={handleOnClick}
-        disabled={isApiWaiting}
-      >
-        Call Public API Route
-      </Button>
-      <p></p>
-      <Button
-        variant="contained"
-        onClick={handleOnClickUser}
-        disabled={isApiWaiting}
-      >
-        Call Fetch Users
-      </Button>
-      <p></p>
-  */}
     </Box>
   );
 };
