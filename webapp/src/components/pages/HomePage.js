a0cf9d6ecd2a7f5f5277afc374df99f03f27d463
TaraSwor <47418494+TaraSwor@users.noreply.github.com>
2024-02-28 12:27:06 -0600

    Dashboard UI Changes (#49)
    Co-authored-by: Joaquin Gimenez <jg@joaquingimenez.com>

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -1,23 +1,118 @@
-import React, { useState } from "react";
+import React from "react";
 // MUI
-import { Box, Typography, Snackbar } from "@mui/material";
-
-import UploadFileCard from "../widgets/UploadFileCard";
+import {
+  Box,
+  Typography,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TableHead,
+  styled,
+} from "@mui/material";
 
 const HomePage = () => {
-  const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
-  const [apiResponse, setApiResponse] = useState("");
+  const DemoPaper = styled(Paper)(({ theme }) => ({
+    width: 800,
+    // height: 500,
+    padding: theme.spacing(2),
+    ...theme.typography.body2,
+    textAlign: "center",
+  }));
+
+  const createData = (Assignment, Deadline) => {
+    return { Assignment, Deadline };
+  };
+
+  const ThisWeekRows = [
+    createData("Module 1 Video", "-"),
+    createData("Module 1 Submit Questions", "March 1, 2023"),
+  ];
+
+  const NextWeekRows = [
+    createData("Module 1 Tag Questions", "March 13, 2023"),
+    createData("Module 1 Vote Questions", "March 17, 2023"),
+  ];
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Typography variant="h4">This is the HomePage component.</Typography>
-      <UploadFileCard />
-      <Snackbar
-        open={isSnackbarOpen}
-        onClose={() => setIsSnackbarOpen(false)}
-        autoHideDuration={3000}
-        message={apiResponse}
-      />
+      <Typography
+        variant="h4"
+        mt={2}
+        textAlign={"center"}
+        sx={{ fontFamily: "monospace" }}
+      >
+        Student Dashboard
+      </Typography>
+
+      <Box
+        display="flex"
+        mt={2}
+        alignItems="center"
+        justifyContent="center"
+        sx={{ flexGrow: 1 }}
+      >
+        <DemoPaper square={false}>
+          <Typography variant="h6">Due This Week</Typography>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 280 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ fontWeight: "bold" }}>Assignment</TableCell>
+                  <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                    Due Date
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {ThisWeekRows.map((row) => (
+                  <TableRow
+                    hover
+                    key={row.Assignment}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.Assignment}
+                    </TableCell>
+                    <TableCell align="right">{row.Deadline}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Typography variant="h6" padding={2}>
+            Upcoming Assignments
+          </Typography>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 280 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell sx={{ fontWeight: "bold" }}>Assignment</TableCell>
+                  <TableCell align="right" sx={{ fontWeight: "bold" }}>
+                    Due Date
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {NextWeekRows.map((row) => (
+                  <TableRow
+                    hover
+                    key={row.Assignment}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.Assignment}
+                    </TableCell>
+                    <TableCell align="right">{row.Deadline}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </DemoPaper>
+      </Box>
     </Box>
   );
 };
