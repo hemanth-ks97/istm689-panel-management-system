66153cc8e17e8eaff9df17045e3e34b5167325dd
Joaquin Gimenez <jg@joaquingimenez.com>
2024-02-28 08:35:30 -0600

    Add upload csv file mui button

--- webapp/src/components/pages/HomePage.js
+++ webapp/src/components/pages/HomePage.js
@@ -1,123 +1,17 @@
 import React, { useState } from "react";
 // MUI
-import { Box, Button, Typography, Snackbar } from "@mui/material";
-// Redux
-import { useSelector } from "react-redux";
-// HTTP Client
-import { httpClient } from "../../client";
-// React Router
-import { useNavigate } from "react-router-dom";
+import { Box, Typography, Snackbar } from "@mui/material";
+
+import UploadFileCard from "../widgets/UploadFileCard";
 
 const HomePage = () => {
-  const navigate = useNavigate();
   const [isSnackbarOpen, setIsSnackbarOpen] = useState(false);
   const [apiResponse, setApiResponse] = useState("");
-  const [isApiWaiting, setIsApiWaiting] = useState(false);
-  const [selectedHowdyFile, SetSelectedHowdyFile] = useState("");
-
-  const { user } = useSelector((state) => state.user);
-
-  const handleOnClick = () => {
-    setIsApiWaiting(true);
-    httpClient
-      .get("/")
-      .then((response) =>
-        setApiResponse(JSON.stringify(response?.data, null, 2))
-      )
-      .catch((err) => setApiResponse(JSON.stringify(err.message, null, 2)))
-      .finally(() => {
-        setIsSnackbarOpen(true);
-        setIsApiWaiting(false);
-      });
-  };
-
-  const handleOnClickUser = () => {
-    setIsApiWaiting(true);
-    httpClient
-      .get("/user", {
-        headers: {
-          Authorization: `Bearer ${user?.token}`,
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) =>
-        setApiResponse(JSON.stringify(response?.data, null, 2))
-      )
-      .catch((err) => setApiResponse(JSON.stringify(err.message, null, 2)))
-      .finally(() => {
-        setIsApiWaiting(false);
-        setIsSnackbarOpen(true);
-      });
-  };
-
-  const sendCSVToServer = (csvData) => {
-    setIsApiWaiting(true);
-    httpClient
-      .post("/howdycsv", csvData, {
-        headers: {
-          Authorization: `Bearer ${user?.token}`,
-          "Content-Type": "text/plain",
-        },
-      })
-      .then((response) =>
-        setApiResponse(JSON.stringify(response?.data, null, 2))
-      )
-      .catch((err) => setApiResponse(JSON.stringify(err.message, null, 2)))
-      .finally(() => {
-        setIsApiWaiting(false);
-        setIsSnackbarOpen(true);
-      });
-  };
-
-  const handleHowdyCSVUpload = () => {
-    if (selectedHowdyFile) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const csvData = e.target.result;
-        sendCSVToServer(csvData);
-      };
-      reader.readAsText(selectedHowdyFile);
-    }
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      SetSelectedHowdyFile(file);
-    }
-  };
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Typography variant="h4">This is the HomePage component.</Typography>
-      <Typography>Test API Call to backend!</Typography>
-      <Button
-        variant="contained"
-        onClick={handleOnClick}
-        disabled={isApiWaiting}
-      >
-        Call Public API Route
-      </Button>
-      <p></p>
-      <Button
-        variant="contained"
-        onClick={handleOnClickUser}
-        disabled={isApiWaiting}
-      >
-        Call Fetch Users
-      </Button>
-      <p></p>
-      <>
-        <input type="file" onChange={handleFileChange} />
-        <Button
-          variant="contained"
-          onClick={handleHowdyCSVUpload}
-          disabled={isApiWaiting}
-        >
-          Upload Howdy CSV
-        </Button>
-      </>
-
+      <UploadFileCard />
       <Snackbar
         open={isSnackbarOpen}
         onClose={() => setIsSnackbarOpen(false)}
---
+++ webapp/src/components/widgets/UploadFileCard.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+// MUI
+
+// HTTP Client
+import { httpClient } from "../../client";
+import { CloudUpload as CloudUploadIcon } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import { styled } from "@mui/material/styles";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
+
+const UploadFileCard = () => {
+  const { user } = useSelector((state) => state.user);
+  const [isApiWaiting, setIsApiWaiting] = useState(false);
+  const [selectedHowdyFile, SetSelectedHowdyFile] = useState("");
+
+  const sendCSVToServer = (csvData) => {
+    setIsApiWaiting(true);
+    httpClient
+      .post("/howdycsv", csvData, {
+        headers: {
+          Authorization: `Bearer ${user?.token}`,
+          "Content-Type": "text/plain",
+        },
+      })
+      .then((response) => console.log(JSON.stringify(response?.data, null, 2)))
+      .catch((err) => console.log(JSON.stringify(err.message, null, 2)))
+      .finally(() => setIsApiWaiting(false));
+  };
+
+  const handleHowdyCSVUpload = () => {
+    if (selectedHowdyFile) {
+      setIsApiWaiting(true);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const csvData = e.target.result;
+        sendCSVToServer(csvData);
+      };
+      reader.readAsText(selectedHowdyFile);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      SetSelectedHowdyFile(file);
+    }
+  };
+
+  return (
+    <Button
+      component="label"
+      role={undefined}
+      variant="contained"
+      tabIndex={-1}
+      disabled={isApiWaiting}
+      onClick={handleHowdyCSVUpload}
+      startIcon={<CloudUploadIcon />}
+    >
+      Upload file
+      <VisuallyHiddenInput type="file" onChange={handleFileChange} />
+    </Button>
+  );
+};
+
+export default UploadFileCard;
