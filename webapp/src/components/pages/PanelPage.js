f540c5b4d6919c23de6a145c78e2d3053f3ea82f
pawan-terdal-tamu <143368089+pawan-terdal-tamu@users.noreply.github.com>
2024-03-02 09:50:16 -0600

    add admin role in frontend and backend (#56)

---
+++ webapp/src/components/pages/PanelPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// MUI
+import { Box, Typography } from "@mui/material";
+
+const PanelPage = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Typography variant="h4">This is where Dr Gomillion will add panel information like start date, end date, topic etc</Typography>
+    </Box>
+  );
+};
+
+export default PanelPage;
