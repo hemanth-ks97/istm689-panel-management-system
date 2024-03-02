352c4ede2180d46f1701c7079f20a334e816487f
naikvaditya <142937246+naikvaditya@users.noreply.github.com>
2024-03-01 23:47:11 -0600

    Added script for panel, question, user, metric

--- terraform/main.tf
+++ terraform/main.tf
@@ -166,7 +166,7 @@ resource "aws_dynamodb_table" "question-dynamodb-table" {
 }
 
 #question table
-resource "aws_dynamodb_table" "question-table" {
+resource "aws_dynamodb_table" "question" {
   name           = "${terraform.workspace}-question"
   billing_mode   = "PROVISIONED"
   read_capacity  = var.dynamodb_table_read_capacity[terraform.workspace]
@@ -192,48 +192,22 @@ resource "aws_dynamodb_table" "question-table" {
   }
 }
 
-#grade table
-resource "aws_dynamodb_table" "grade-table" {
-  name           = "${terraform.workspace}-grade"
+#panel table
+resource "aws_dynamodb_table" "panel" {
+  name           = "${terraform.workspace}-panel"
   billing_mode   = "PROVISIONED"
   read_capacity  = var.dynamodb_table_read_capacity[terraform.workspace]
   write_capacity = var.dynamodb_table_write_capacity[terraform.workspace]
-  hash_key       = "GradeID"
-
-  attribute {
-    name = "GradeID"
-    type = "S"
-  }
-
-  attribute {
-    name = "StudentID"
-    type = "S"
-  }
+  hash_key       = "PanelID"
 
   attribute {
     name = "PanelID"
     type = "S"
   }
-
-  global_secondary_index {
-    name            = "StudentIDIndex"
-    hash_key        = "StudentID"
-    projection_type = "ALL"
-    read_capacity   = var.dynamodb_global_secondary_idx_read_capacity[terraform.workspace]
-    write_capacity  = var.dynamodb_global_secondary_idx_write_capacity[terraform.workspace]
-  }
-
-  global_secondary_index {
-    name            = "PanelIDIndex"
-    hash_key        = "PanelID"
-    projection_type = "ALL"
-    read_capacity   = var.dynamodb_global_secondary_idx_read_capacity[terraform.workspace]
-    write_capacity  = var.dynamodb_global_secondary_idx_write_capacity[terraform.workspace]
-  }
 }
 
 #user table
-resource "aws_dynamodb_table" "user-table" {
+resource "aws_dynamodb_table" "user" {
   name           = "${terraform.workspace}-user"
   billing_mode   = "PROVISIONED"
   read_capacity  = var.dynamodb_table_read_capacity[terraform.workspace]
@@ -246,18 +220,14 @@ resource "aws_dynamodb_table" "user-table" {
   }
 }
 
-#engagement table
-resource "aws_dynamodb_table" "engagement-table" {
-  name           = "${terraform.workspace}-engagement"
+#metrics table
+resource "aws_dynamodb_table" "metric" {
+  name           = "${terraform.workspace}-metric"
   billing_mode   = "PROVISIONED"
   read_capacity  = var.dynamodb_table_read_capacity[terraform.workspace]
   write_capacity = var.dynamodb_table_write_capacity[terraform.workspace]
-  hash_key       = "EngagementID"
-
-  attribute {
-    name = "EngagementID"
-    type = "S"
-  }
+  hash_key       = "StudentID"
+  range_key      = "PanelID"
 
   attribute {
     name = "StudentID"
@@ -272,6 +242,7 @@ resource "aws_dynamodb_table" "engagement-table" {
   global_secondary_index {
     name            = "StudentIDIndex"
     hash_key        = "StudentID"
+    range_key       = "PanelID"
     projection_type = "ALL"
     read_capacity   = var.dynamodb_global_secondary_idx_read_capacity[terraform.workspace]
     write_capacity  = var.dynamodb_global_secondary_idx_write_capacity[terraform.workspace]
@@ -280,6 +251,7 @@ resource "aws_dynamodb_table" "engagement-table" {
   global_secondary_index {
     name            = "PanelIDIndex"
     hash_key        = "PanelID"
+    range_key       = "StudentID"
     projection_type = "ALL"
     read_capacity   = var.dynamodb_global_secondary_idx_read_capacity[terraform.workspace]
     write_capacity  = var.dynamodb_global_secondary_idx_write_capacity[terraform.workspace]
