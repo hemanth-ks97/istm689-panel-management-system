ff1886ef136f67fc4153dcd3c324138b3c930b4d
Joaquin Gimenez <jg@joaquingimenez.com>
2024-03-06 13:01:41 -0600

    Add some spacing

--- webapp/src/components/pages/AdminImportPage.js
+++ webapp/src/components/pages/AdminImportPage.js
@@ -6,7 +6,7 @@ import UploadFileCard from "../widgets/UploadFileCard";
 const AdminImportPage = () => {
   return (
     <Typography>
-      Upload the files to create new students!
+      Upload the files to create or update students
       <UploadFileCard />
     </Typography>
   );
