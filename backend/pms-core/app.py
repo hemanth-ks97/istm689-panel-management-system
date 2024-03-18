53ab3ef728926bb97e4a38dabbce033f2dd4163c
Joaquin Gimenez <jg@joaquingimenez.com>
2024-03-18 18:25:25 -0500

    Lint python

--- backend/pms-core/app.py
+++ backend/pms-core/app.py
@@ -1,4 +1,5 @@
 """Main application file for the PMS Core API."""
+
 import itertools
 from collections import Counter
 
@@ -351,28 +352,30 @@ def get_user(id):
 
 
 @app.route(
-    "/panel/{panel_id}/distribute",
+    "/panel/{id}/distribute",
     methods=["GET"],
     authorizer=token_authorizer,
 )
-def distribute_tag_questions(panel_id):
+def distribute_tag_questions(id):
     try:
         # Get list of all questions for that panel from the usersDB
-        questions = get_questions_by_panel(panel_id)
+        questions = get_questions_by_panel(id)
 
         # Creating map to store questionID and corresponding userID
         question_id_user_id_map = {}
 
         # Get all questionIDs and corresponding UserID from questions
         for question in questions:
-            question_id_user_id_map[question.get('QuestionID')] = question.get('UserID')
+            question_id_user_id_map[question.get("QuestionID")] = question.get("UserID")
 
         # Store all questionIDs from Map
         question_ids = list(question_id_user_id_map.keys())
 
         # Get total students from the usersDB
         student_ids = list(get_user_db().get_student_user_ids())
-        number_of_questions_per_student = get_panel_db().get_number_of_questions_by_panel_id(panel_id)[0]
+        number_of_questions_per_student = (
+            get_panel_db().get_number_of_questions_by_panel_id(id)[0]
+        )
         number_of_questions = len(question_ids)
         number_of_students = len(student_ids)
         number_of_question_slots = number_of_questions_per_student * number_of_students
@@ -392,7 +395,9 @@ def distribute_tag_questions(panel_id):
 
         # Append each question id to the list with repetitions
         for question_id in question_ids:
-            distributed_question_id_slots.extend([question_id] * min_repetition_of_questions)
+            distributed_question_id_slots.extend(
+                [question_id] * min_repetition_of_questions
+            )
 
         # Fill remaining slots with top question ids and append to the list
         if number_of_extra_question_slots > 0:
@@ -416,7 +421,9 @@ def distribute_tag_questions(panel_id):
                 question_id = distributed_question_id_slots.pop(0)
 
                 # Check uniqueness in the sublist and check if question was entered by user
-                while (question_id in question_id_sublist) or (student_id == question_id_user_id_map.get(question_id)):
+                while (question_id in question_id_sublist) or (
+                    student_id == question_id_user_id_map.get(question_id)
+                ):
 
                     # Append it to the end of the master list and fetch the next question
                     distributed_question_id_slots.append(question_id)
@@ -430,7 +437,9 @@ def distribute_tag_questions(panel_id):
             # Assign the sublist to the next available student ID
             student_id_question_ids_map[student_id] = question_id_sublist
 
-        question_id_repetition_count_map = Counter(itertools.chain.from_iterable(student_id_question_ids_map.values()))
+        question_id_repetition_count_map = Counter(
+            itertools.chain.from_iterable(student_id_question_ids_map.values())
+        )
 
         # TODO - Add the student_question_map to an S3 bucket
 
