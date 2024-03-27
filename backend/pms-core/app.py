7fb2ec6ccc51abe73e5295e702d3bb675daac0c9
TaraSwor <anshitag1999@gmail.com>
2024-03-26 22:46:37 -0500

    Added logic for submit questions grading

--- backend/pms-core/app.py
+++ backend/pms-core/app.py
@@ -1,5 +1,6 @@
 """Main application file for the PMS Core API."""
 
+from decimal import Decimal
 from itertools import chain
 from collections import Counter, defaultdict
 
@@ -59,6 +60,28 @@ _USER_DB = None
 _QUESTION_DB = None
 _PANEL_DB = None
 _METRIC_DB = None
+
+#Score for only submitting questions
+submit_score = 10 
+#Points if question is between +1 and -1 std deviation of likes
+std_question_score = 10
+#Points if question is above +1 std deviation of likes
+above_std_score = 5
+#Points for overall engagement during tagging
+engagement_score_tag = 10
+#Points for overall engagement during voting
+engagement_score_vote = 10
+#Points for tagging all questions
+tagging_score = 30  
+#Points for voting all questions
+voting_score = 30
+#Points for question if it is in voting stage
+extra_voting_score = 5
+#Points if question is selected in top 10 of voting
+top_questions_score = 10
+
+total_score = 0
+#Grade = (Questions *0.2) + (Tagging * .3) + (Voting * .3) + (Engagement * .2)
 
 
 def get_user_db():
@@ -705,28 +728,50 @@ def post_question_batch():
         raw_questions = incoming_json["questions"]
 
         new_questions = []
+
         for question in raw_questions:
             # Build Question object for database
-            new_question = {
-                "QuestionID": str(uuid4()),
-                "UserID": user_id,
-                "PanelID": incoming_json["panelId"],
-                "QuestionText": question,
-                "CreatedAt": datetime.now(timezone.utc).isoformat(timespec="seconds"),
-                "DislikedBy": [],
-                "LikedBy": [],
-                "NeutralizedBy": [],
-                "DislikeScore": -1,
-                "FinalScore": -1,
-                "LikeScore": -1,
-                "NeutralScore": -1,
-                "PresentationBonusScore": -1,
-                "VotingStageBonusScore": -1,
-            }
+            if question != "":
+                new_question = {
+                    "QuestionID": str(uuid4()),
+                    "UserID": user_id,
+                    "PanelID": panel_id,
+                    "QuestionText": question,
+                    "CreatedAt": datetime.now(timezone.utc).isoformat(timespec="seconds"),
+                    "DislikedBy": [],
+                    "LikedBy": [],
+                    "NeutralizedBy": [],
+                    "DislikeScore": -1,
+                    "FinalScore": -1,
+                    "LikeScore": -1,
+                    "NeutralScore": -1,
+                    "PresentationBonusScore": -1,
+                    "VotingStageBonusScore": -1,
+                }
 
-            new_questions.append(new_question)
+                new_questions.append(new_question)
 
         get_question_db().add_questions_batch(new_questions)
+        # #Check if all questions have been submitted
+        no_of_questions = get_panel_db().get_number_of_questions_by_panel_id(panel_id)
+        #Add score to metrics
+        if no_of_questions == len(new_questions):
+            metric_for_submit ={
+                "UserID": user_id,
+                "PanelID": panel_id,
+                "CreatedAt": datetime.now(timezone.utc).isoformat(timespec="seconds"),
+                "EnteredQuestionsTotalScore": Decimal(submit_score)
+            }
+        else:
+            sub_score_for_questions = round((len(new_questions)/no_of_questions[0])*submit_score)
+            metric_for_submit ={
+                "UserID": user_id,
+                "PanelID": panel_id,
+                "CreatedAt": datetime.now(timezone.utc).isoformat(timespec="seconds"),
+                "EnteredQuestionsTotalScore": Decimal(sub_score_for_questions)
+            }
+        get_metric_db().add_metric(metric_for_submit)
+        
         # Returns the result of put_item, kind of metadata and stuff
         return {
             "message": "Questions successfully inserted in the DB",
