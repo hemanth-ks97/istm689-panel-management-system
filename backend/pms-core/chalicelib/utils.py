10a83de456d153d4272bb4319de89d2f347f4d51
Joaquin Gimenez <jg@joaquingimenez.com>
2024-04-02 17:43:59 -0500

    test code (#110)

--- backend/pms-core/chalicelib/utils.py
+++ backend/pms-core/chalicelib/utils.py
@@ -1,17 +1,16 @@
+import boto3
+
+from random import shuffle
 from datetime import datetime, timezone, timedelta
 from jwt import decode, get_unverified_header, encode
 from google.auth import exceptions
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from uuid import uuid4
-
-
-import boto3
-
-
+from chalicelib import db
 from json import dumps, loads
 
-from .constants import GOOGLE_ISSUER, BOTO3_S3_TYPE
+from .constants import GOOGLE_ISSUER, BOTO3_S3_TYPE, BOTO3_DYNAMODB_TYPE
 
 from .config import (
     JWT_SECRET,
@@ -19,9 +18,38 @@ from .config import (
     JWT_AUDIENCE,
     JWT_ISSUER,
     JWT_TOKEN_EXPIRATION_DAYS,
+    PANELS_BUCKET_NAME,
+    USER_TABLE_NAME,
+    QUESTION_TABLE_NAME,
 )
 
 s3_client = boto3.client(BOTO3_S3_TYPE)
+_USER_DB = None
+_QUESTION_DB = None
+
+
+def get_user_db():
+    global _USER_DB
+    try:
+        if _USER_DB is None:
+            _USER_DB = db.DynamoUserDB(
+                boto3.resource(BOTO3_DYNAMODB_TYPE).Table(USER_TABLE_NAME)
+            )
+    except Exception as e:
+        return {"error": str(e)}
+    return _USER_DB
+
+
+def get_question_db():
+    global _QUESTION_DB
+    try:
+        if _QUESTION_DB is None:
+            _QUESTION_DB = db.DynamoQuestionDB(
+                boto3.resource(BOTO3_DYNAMODB_TYPE).Table(QUESTION_TABLE_NAME)
+            )
+    except Exception as e:
+        return {"error": str(e)}
+    return _QUESTION_DB
 
 
 def _generate_id():
@@ -212,3 +240,102 @@ def get_current_time_utc():
     return (
         datetime.now(timezone.utc).isoformat(timespec="seconds").replace("+00:00", "Z")
     )
+
+
+def distribute_tag_questions(panel_id):
+    try:
+        # Get list of all questions for that panel from the usersDB
+        questions = get_question_db().get_questions_by_panel(panel_id)
+
+        # Creating map to store questionID and corresponding userID
+        question_map = {}
+
+        # Get all questionIDs and corresponding UserID from questions
+        for question in questions:
+            question_id = question.get("QuestionID")
+            user_id = question.get("UserID")
+            question_text = question.get("QuestionText")
+            question_map[question_id] = {
+                "UserID": user_id,
+                "QuestionText": question_text,
+            }
+
+        # Store all questionIDs from Map
+        question_ids = list(question_map.keys())
+
+        # Get total students from the usersDB
+        student_ids = list(get_user_db().get_student_user_ids())
+        number_of_questions_per_student = 10
+        number_of_questions = len(question_ids)
+        number_of_students = len(student_ids)
+        number_of_question_slots = number_of_questions_per_student * number_of_students
+        number_of_repetition_of_questions = (
+            number_of_question_slots // number_of_questions
+        )
+        number_of_extra_question_slots = number_of_question_slots % number_of_questions
+
+        # Print variable values
+        print("Panel ID: ", id)
+        print("Number of questions per student: ", number_of_questions_per_student)
+        print("Total number of questions: ", number_of_questions)
+        print("Total number of students: ", number_of_students)
+        print("Total number of question slots: ", number_of_question_slots)
+        print("Number of extra question slots: ", number_of_extra_question_slots)
+
+        # Distribute questions to slots
+        distributed_question_id_slots = []
+
+        # Append each question id to the list with repetitions
+        for question_id in question_ids:
+            distributed_question_id_slots.extend(
+                [question_id] * number_of_repetition_of_questions
+            )
+
+        # Fill remaining slots with top question ids and append to the list
+        if number_of_extra_question_slots > 0:
+            top_questions = question_ids[:number_of_extra_question_slots]
+            distributed_question_id_slots.extend(top_questions)
+
+        # Shuffle the question slots to randomize the order
+        shuffle(distributed_question_id_slots)
+
+        # Create a collection to store questionSubLists
+        student_id_questions_map = {}
+
+        for _ in range(number_of_students):
+            student_id = student_ids.pop(0)
+
+            # Create a sublist for each iteration
+            question_id_text_map = {}
+
+            # Pop questions from the questions slot list to put in the map
+            for _ in range(number_of_questions_per_student):
+                question_id = distributed_question_id_slots.pop(0)
+
+                # Check if question exists in the map keys and check if question was entered by user
+                while (question_id in question_id_text_map.keys()) or (
+                    student_id == question_map[question_id]["UserID"]
+                ):
+                    # Append it to the end of the master list and fetch the next question
+                    distributed_question_id_slots.append(question_id)
+
+                    # Get next question from the questionID slot list
+                    question_id = distributed_question_id_slots.pop(0)
+
+                # Add question to map
+                question_id_text_map[question_id] = question_map[question_id][
+                    "QuestionText"
+                ]
+
+            # Assign the sublist to the next available student ID
+            student_id_questions_map[student_id] = question_id_text_map
+
+        # Add the student_question_map to an S3 bucket
+
+        upload_objects(
+            PANELS_BUCKET_NAME, id, "questions.json", student_id_questions_map
+        )
+
+        return student_id_questions_map
+    except Exception as e:
+        return {"error": str(e)}
