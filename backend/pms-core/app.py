893687d14e300dd2774ff73c0d8c6566b46081af
TaraSwor <anshitag1999@gmail.com>
2024-04-07 17:37:56 -0500

    Resolving errors

--- backend/pms-core/app.py
+++ backend/pms-core/app.py
@@ -1,12 +1,6 @@
 """Main application file for the PMS Core API."""
 
-<<<<<<< HEAD
-=======
 from decimal import Decimal
-from itertools import chain
-from collections import Counter, defaultdict
-
->>>>>>> 7fb2ec6 (Added logic for submit questions grading)
 import requests
 import boto3
 import pandas as pd
@@ -82,61 +76,6 @@ from chalicelib.database.db_provider import (
 
 
 app = Chalice(app_name=f"{ENV}-pms-core")
-<<<<<<< HEAD
-=======
-_USER_DB = None
-_QUESTION_DB = None
-_PANEL_DB = None
-_METRIC_DB = None
-
-
-def get_user_db():
-    global _USER_DB
-    try:
-        if _USER_DB is None:
-            _USER_DB = db.DynamoUserDB(
-                boto3.resource(BOTO3_DYNAMODB_TYPE).Table(USER_TABLE_NAME)
-            )
-    except Exception as e:
-        return {"error": str(e)}
-    return _USER_DB
-
-
-def get_question_db():
-    global _QUESTION_DB
-    try:
-        if _QUESTION_DB is None:
-            _QUESTION_DB = db.DynamoQuestionDB(
-                boto3.resource(BOTO3_DYNAMODB_TYPE).Table(QUESTION_TABLE_NAME)
-            )
-    except Exception as e:
-        return {"error": str(e)}
-    return _QUESTION_DB
-
-
-def get_panel_db():
-    global _PANEL_DB
-    try:
-        if _PANEL_DB is None:
-            _PANEL_DB = db.DynamoPanelDB(
-                boto3.resource(BOTO3_DYNAMODB_TYPE).Table(PANEL_TABLE_NAME)
-            )
-    except Exception as e:
-        return {"error": str(e)}
-    return _PANEL_DB
-
-
-def get_metric_db():
-    global _METRIC_DB
-    try:
-        if _METRIC_DB is None:
-            _METRIC_DB = db.DynamoMetricDB(
-                boto3.resource(BOTO3_DYNAMODB_TYPE).Table(METRIC_TABLE_NAME)
-            )
-    except Exception as e:
-        return {"error": str(e)}
-    return _METRIC_DB
->>>>>>> 7fb2ec6 (Added logic for submit questions grading)
 
 
 def dummy():
@@ -730,32 +669,13 @@ def post_question_batch():
         new_questions = []
 
         for question in raw_questions:
-            # Build Question object for database
-<<<<<<< HEAD
-            new_question = {
-                "QuestionID": generate_question_id(),
-                "UserID": user_id,
-                "PanelID": incoming_json["panelId"],
-                "QuestionText": question,
-                "CreatedAt": get_current_time_utc(),
-                "DislikedBy": [],
-                "LikedBy": [],
-                "NeutralizedBy": [],
-                "DislikeScore": -1,
-                "FinalScore": -1,
-                "LikeScore": -1,
-                "NeutralScore": -1,
-                "PresentationBonusScore": -1,
-                "VotingStageBonusScore": -1,
-            }
-=======
             if question != "":
                 new_question = {
-                    "QuestionID": str(uuid4()),
+                    "QuestionID": generate_question_id(),
                     "UserID": user_id,
-                    "PanelID": panel_id,
+                    "PanelID": incoming_json["panelId"],
                     "QuestionText": question,
-                    "CreatedAt": datetime.now(timezone.utc).isoformat(timespec="seconds"),
+                    "CreatedAt": get_current_time_utc(),
                     "DislikedBy": [],
                     "LikedBy": [],
                     "NeutralizedBy": [],
@@ -766,12 +686,10 @@ def post_question_batch():
                     "PresentationBonusScore": -1,
                     "VotingStageBonusScore": -1,
                 }
->>>>>>> 7fb2ec6 (Added logic for submit questions grading)
 
                 new_questions.append(new_question)
 
         get_question_db().add_questions_batch(new_questions)
-<<<<<<< HEAD
 
         pretty_time = datetime.now(timezone.utc).strftime("%m/%d/%Y at %H:%M:%S UTC")
 
@@ -794,8 +712,6 @@ def post_question_batch():
         html_body += "<p>Remember that your questions will be reviewed.</p>"
         html_body += "<p>Best regards,<br/>"
         html_body += "PMS team</p>"
-
-=======
         # #Check if all questions have been submitted
         no_of_questions = get_panel_db().get_number_of_questions_by_panel_id(panel_id)
         #Add score to metrics
@@ -816,7 +732,6 @@ def post_question_batch():
             }
         get_metric_db().add_metric(metric_for_submit)
         
->>>>>>> 7fb2ec6 (Added logic for submit questions grading)
         # Returns the result of put_item, kind of metadata and stuff
         send_email(
             destination_addresses=["davidgomilliontest@gmail.com"],
@@ -1189,7 +1104,6 @@ def get_all_metrics_():
 
     return metrics
 
-<<<<<<< HEAD
 
 @app.route(
     "/panel/{id}/questions/tagging",
@@ -1473,7 +1387,7 @@ def get_final_question_list(id):
 
     except Exception as e:
         return {"error": str(e)}
-=======
+    
 @app.route(
     "/metric/final",
     methods=["POST"],
@@ -1577,5 +1491,3 @@ def post_grades():
         )
     except Exception as e:
         return {"error": str(e)}
-        
->>>>>>> a03387d (Added framework for grading)
